84dca5ed8b02015d4e16a8e2ce7e3160a9daff23
Yash <yg232004@gmail.com>
2024-01-29 01:38:04 +0530

    validated email and pass

--- src/components/login.js
+++ src/components/login.js
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
-
+import React, { useRef, useState } from "react";
+import { checkData } from "../utils/Validation";
 const Login = () => {
   const backgroundImageUrl =
     "https://assets.nflxext.com/ffe/siteui/vlv3/32c47234-8398-4a4f-a6b5-6803881d38bf/eed3a573-8db7-47ca-a2ce-b511e0350439/IN-en-20240122-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
   const [isSignIn, setIsSignIn] = useState(true);
-
+  const [errorMessage, setErrorMessage] = useState(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  const handleClick = () => {
+    const message = checkData(email.current.value, password.current.value);
+    setErrorMessage(message);
+  };
   const handleSignIn = () => {
     setIsSignIn(!isSignIn);
   };
@@ -21,8 +27,11 @@ const Login = () => {
           alt="logo"
           className="w-56 bg-gradient-to-b from-black"
         />
-        <div className="relative mt-20 w-full">
-          <form className="bg-black bg-opacity-60 p-8 sm:p-12 lg:p-20 max-w-md mx-auto flex flex-col">
+        <div className="relative my-16 w-full">
+          <form
+            onSubmit={(e) => e.preventDefault()}
+            className="bg-black bg-opacity-60 p-8 mb-10 sm:p-12 lg:p-20 max-w-sm mx-auto flex flex-col"
+          >
             <h1 className="text-white text-3xl font-bold mb-6 ">
               {isSignIn ? "Sign In" : "Sign Up"}
             </h1>
@@ -34,16 +43,20 @@ const Login = () => {
               />
             )}
             <input
-              type="email"
+              ref={email}
+              type="text"
               placeholder="Email address"
               className="border border-white/75 bg-black bg-opacity-60  rounded-sm text-white p-3 mb-4 "
             />
             <input
+              ref={password}
               type="password"
               placeholder="Password"
               className="border border-white/75 bg-black bg-opacity-60 text-white p-3 mb-6 "
             />
+            <p className="text-red-500 text-sm m-auto mb-2">{errorMessage}</p>
             <button
+              onClick={handleClick}
               className="bg-red-700 text-white p-3 rounded-md font-bold"
               type="submit"
             >
