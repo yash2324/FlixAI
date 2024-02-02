687f50821b736f41619d60c54042a5a6c8e07433
Yash <yg232004@gmail.com>
2024-02-02 23:17:51 +0530

    added redux to store now playing movies

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -2,8 +2,10 @@ import React, { useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import { options } from "../utils/constants";
-
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/movieSlice";
 const Browse = () => {
+  const dispatch = useDispatch();
   const getNowPlayingMovies = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing?page=1",
@@ -12,6 +14,7 @@ const Browse = () => {
 
     const json = await data.json();
     console.log(json);
+    dispatch(addNowPlayingMovies(json.results));
   };
 
   useEffect(() => {
