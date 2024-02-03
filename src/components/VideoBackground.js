86b34c4e17aa19c3dddb39e58b6ee51497ae4fb2
Yash <yg232004@gmail.com>
2024-02-03 23:57:21 +0530

    fetching for video background

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { options } from "../utils/constants";
 
-const VideoBackground = () => {
+const VideoBackground = ({ id }) => {
+  const getMovieVideos = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/572802/videos?language=en-US",
+      options
+    );
+    const json = await data.json();
+    console.log(json?.results);
+  };
+  useEffect(() => {
+    getMovieVideos();
+  }, []);
   return (
     <>
       <div>VideoBackground</div>
