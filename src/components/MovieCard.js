76e590f19628d87d089bc6e67552ae4491220316
Yash <yg232004@gmail.com>
2024-02-04 20:27:43 +0530

    made the cards ui better

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { IMG_TMDB } from "../utils/constants";
+
 const MovieCard = ({ movie }) => {
   return (
     <>
-      <div className="w-48 px-4">
-        <img alt="movie poster" src={IMG_TMDB + movie.poster_path} />
+      <div className="relative w-48 px-4 overflow-hidden transition-transform transform hover:scale-110 cursor-pointer">
+        <img
+          alt="movie poster"
+          src={IMG_TMDB + movie.poster_path}
+          className="w-full h-auto"
+        />
       </div>
     </>
   );
